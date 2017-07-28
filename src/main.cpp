3e4b17fd685cd97cf4ea70073a7f4ca240d4afaa
yangzhiyuan <yangzhiyuan0928@163.com>
2017-07-28 08:48:40 +0800

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,210 @@
+#include <stdint.h>
+#include <getopt.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <cassert>
+#include <sys/epoll.h>
+#include <pthread.h>
+#include <iostream>
+#include <vector>
+
+#include "epoll.h"
+#include "error.h"
+#include "debug.h"
+#include "util.h"
+#include "threadpool.h"
+#include "timer.h"
+#include "http.h"
+
+#define MAX_FD 32  //
+#define MAX_EVENT_NUMBER 10000
+#define CONF "http.conf"
+#define PROGRAM_VERSION "0.1"
+
+using std::vector;
+extern struct epoll_event *events;
+
+static void usage()
+{
+	fprintf(stderr,
+		"httpserver [option]...\n"
+		"  -c|--conf <config file>	Specify config file. Default ./http.conf.\n"
+		"  -?|-h|--help 			Help information.\n"
+		"  -V|--version 			Display program version.\n"
+		);
+}
+
+static const struct option long_options[]=
+{
+    {"help",no_argument,NULL,'?'},
+    {"version",no_argument,NULL,'V'},
+    {"conf",required_argument,NULL,'c'},
+    {NULL,0,NULL,0}
+};
+
+void http_close_conn()
+{
+
+}
+
+
+int main( int argc, char* argv[] )
+{
+	char *config = CONF;
+	int rc, opt = 0, options_index = 0;
+    if( argc == 1 )
+    {
+		usage();
+		exit(1);
+    }
+    while ((opt=getopt_long(argc, argv,"Vc:?h",long_options,&options_index)) != EOF)
+	{
+	    switch (opt)
+		{
+	        case  0 : break;
+	        case 'c':
+	            config = optarg;
+	            break;
+	        case 'V':
+	            printf(PROGRAM_VERSION "\n");
+	            return 0;
+	        case ':':
+	        case 'h':
+	        case '?':
+	            usage();
+	            return 0;
+	    }
+    }
+	debug("Config file: %s", config);
+    if (optind < argc) {
+        log_err("non-option argv-elements: ");
+        while (optind < argc)
+            log_err("%s ", argv[optind++]);
+        return 0;
+    }
+
+    char conf_buf[BUFSIZE];
+    http_conf_t cf;
+    rc = read_conf(config, &cf, conf_buf, BUFSIZE);
+    check(rc == CONFIG_OK, "read conf err");
+
+    addsig( SIGPIPE, SIG_IGN, true );
+
+    threadpool< http_conn >* pool = NULL; //创建线程池，处理http请求
+    try
+    {
+        pool = new threadpool< http_conn >;
+    }
+    catch( ... )
+    {
+        return -1;
+    }
+
+    //http_conn *users = new http_conn[ MAX_FD ];
+    //assert( users );
+
+    int listenfd;
+    struct sockaddr_in clientaddr;
+    socklen_t inlen = 1;
+    memset(&clientaddr, 0, sizeof(struct sockaddr_in));
+    listenfd = open_listenfd(cf.port);
+    assert( listenfd >= 0 );
+
+    int epollfd = http_epoll_create(0);  //内核事件表
+    assert( epollfd != -1 );
+
+    http_epoll_add( epollfd, listenfd, false );  //将listenfd设置为非阻塞IO,并注册事件到epollfd
+	rc = make_socket_non_blocking(listenfd);
+	check(rc == 0, "make_socket_non_blocking");
+    http_conn::m_epollfd = epollfd;
+    vector<http_conn> users;
+
+	time_heap *http_timer = new time_heap(); //
+
+	while( true )
+    {
+        int msec = http_timer->get_top_msec();
+        int number = epoll_wait( epollfd, events, MAX_EVENT_NUMBER, msec );
+        if ( ( number < 0 ) && ( errno != EINTR ) )
+        {
+            printf( "epoll failure\n" );
+            break;
+        }
+        http_timer->tick();  //handler expire timers
+        for ( int i = 0; i < number; i++ )
+        {
+            int sockfd = events[i].data.fd;
+            if( sockfd == listenfd )  //客户端新连接请求
+            {
+                int connfd;
+                while(1)
+                {
+                    connfd = accept( listenfd, ( struct sockaddr* )&clientaddr, &inlen );
+                    if(connfd < 0)
+                    {
+                        if((errno == EAGAIN) || (errno == EWOULDBLOCK))
+                            break;
+                        else
+                        {
+                            log_err("accept");
+                            break;
+                        }
+                        if( http_conn::m_user_count >= MAX_FD )
+                        {
+                            show_error( connfd, "Internal server busy" );
+                            continue;
+                        }
+                        http_conn user;  //
+                        user.init( connfd, clientaddr );
+                        users.push_back(user);
+
+                        struct timer_node timer(TIMEOUT_DEFAULT, http_close_conn);
+                        http_timer->add_timer(timer);
+                    }
+                }
+            }
+            else if( events[i].events & ( EPOLLRDHUP | EPOLLHUP | EPOLLERR ) )  //本端出错会触发EPOLLERR和EPOLLHUP
+            {
+                log_err("epoll error fd: %d", sockfd);
+                close(sockfd);
+                continue;
+            }
+
+            else if( events[i].events & EPOLLIN )  //有数据可读
+            {
+                if( users[sockfd].read() )  //主线程读出数据，然后往请求队列中添加任务，唤醒等待线程，进行处理
+                {
+                    pool->append( &users[sockfd] );
+                }
+                else
+                {
+                    users[sockfd].close_conn();
+                }
+            }
+            else if( events[i].events & EPOLLOUT )  //有数据可写
+            {
+                if( !users[sockfd].write() )
+                {
+                    users[sockfd].close_conn();
+                }
+            }
+            else
+            {}
+        }
+    }
+
+    close( epollfd );
+    close( listenfd );
+    delete http_timer;
+    delete pool;
+    return 0;
+}
+
---
+++ src/util.cpp
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <stdint.h>
+#include <getopt.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <cassert>
+#include <sys/epoll.h>
+#include "util.h"
+#include "debug.h"
+#include "locker.h"
+#include "epoll.h"
+#include "error.h"
+#include "debug.h"
+#include "util.h"
+#include "threadpool.h"
+#include "timer.h"
+#include "http.h"
+
+int make_socket_non_blocking(int fd)
+{
+  int flags, s;
+  flags = fcntl(fd, F_GETFL, 0);
+  if (flags == -1) {
+      log_err("fcntl");
+      return -1;
+  }
+  flags |= O_NONBLOCK;
+  s = fcntl(fd, F_SETFL, flags);
+  if (s == -1) {
+      log_err("fcntl");
+      return -1;
+  }
+  return 0;
+}
+
+void addsig( int sig, void( handler )(int), bool restart = true )
+{
+    struct sigaction sa;
+    memset( &sa, '\0', sizeof( sa ) );
+    sa.sa_handler = handler;
+    if( restart )
+    {
+        sa.sa_flags |= SA_RESTART;
+    }
+    sigfillset( &sa.sa_mask );
+    assert( sigaction( sig, &sa, NULL ) != -1 );
+}
+
+void show_error( int connfd, const char* info )
+{
+    printf( "%s", info );
+    send( connfd, info, strlen( info ), 0 );
+    close( connfd );
+}
+
+int read_conf(char *filename, http_conf_t *cf, char *buf, int len)
+{
+    FILE *fp = fopen(filename, "r");
+    if (!fp) {
+        log_err("cannot open config file: %s", filename);
+        return CONFIG_ERROR;
+    }
+
+    int pos = 0;
+    char *delim_pos;
+    int line_len;
+    char *cur_pos = buf + pos;
+
+    while (fgets(cur_pos, len-pos, fp)) {
+        delim_pos = strstr(cur_pos, DELIM);
+        line_len = strlen(cur_pos);
+
+        if (!delim_pos)
+            return CONFIG_ERROR;
+
+        if (cur_pos[strlen(cur_pos) - 1] == '\n') {
+            cur_pos[strlen(cur_pos) - 1] = '\0';
+        }
+
+        if (strncmp("root", cur_pos, 4) == 0) {
+            cf->root = delim_pos + 1; //cf->root == "root"
+        }
+
+        if (strncmp("port", cur_pos, 4) == 0) {
+            cf->port = atoi(delim_pos + 1); //cf->port == 3000
+        }
+
+        if (strncmp("threadnum", cur_pos, 9) == 0) {
+            cf->thread_num = atoi(delim_pos + 1); //cf->thread_num == 4
+        }
+
+        cur_pos += line_len;
+    }
+
+    fclose(fp);
+    return CONFIG_OK;
+}
+
+int open_listenfd(int port)
+{
+	if(port <= 0)
+		port = 8080;
+	int listenfd, optval;
+
+	if((listenfd = socket(AF_INET, SOCK_STREAM,0)) < 0)
+		return -1;
+    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(int)) < 0)
+  		return -1;
+    struct linger tmp = { 1, 0 };
+    if(setsockopt( listenfd, SOL_SOCKET, SO_LINGER, &tmp, sizeof( tmp )) < 0 )
+		return -1;
+	struct sockaddr_in serveraddr;
+    bzero( &serveraddr, sizeof( serveraddr ) );
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons((unsigned short)port);
+    if (bind(listenfd, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) < 0)
+	    return -1;
+    if (listen(listenfd, LISTENQ) < 0)
+	    return -1;
+
+    return listenfd;
+}
+
+int64_t get_currentmsec()
+{
+    struct timeval cur_time;
+    int rc = gettimeofday(&cur_time, NULL);
+    check(rc == 0, "gettimeofday error");
+    int64_t current_msec = cur_time.tv_sec * 1000 + cur_time.tv_usec/1000;  //ms
+    return current_msec;
+}
+
